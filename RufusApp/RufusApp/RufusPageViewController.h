ad96474d385c9290cb6a6b560c783fa342c222b0
Jeremy Stewart <jlstewart379@gmail.com>
2013-08-15 14:31:48 -0400

    Initial commit of Rufus project

---
+++ RufusApp/RufusApp/RufusPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  RufusPageViewController.h
+//  RufusApp
+//
+//  Created by Jeremy Stewart on 8/15/13.
+//  Copyright (c) 2013 Northwoods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RufusPageViewController : UIViewController
+
+@end
---
+++ RufusApp/RufusApp/RufusPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  RufusPageViewController.m
+//  RufusApp
+//
+//  Created by Jeremy Stewart on 8/15/13.
+//  Copyright (c) 2013 Northwoods. All rights reserved.
+//
+
+#import "RufusPageViewController.h"
+
+@interface RufusPageViewController ()
+
+@end
+
+@implementation RufusPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
