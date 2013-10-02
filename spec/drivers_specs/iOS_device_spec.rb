56b8c91834046e77088ced4b6cb105dcabd1c72d
Jeremy Stewart <jlstewart379@gmail.com>
2013-10-01 23:54:04 -0400

    tests for IOS_Device

---
+++ spec/drivers_specs/iOS_device_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'rufus/drivers/iOS_device'
+require 'selenium-webdriver'
+
+
+describe Rufus::Drivers::IOS_Device do
+
+  let(:config){ {"browser" =>"iOS", "platform"=>"Mac", "version"=>"6.1", "app"=>"/Users/app/path/rufus.app", "use_physical" => "true"}}
+  let(:capabilities){{"browserName" =>"iOS", "platform"=>"Mac", "version"=>"6.1", "app"=>"/Users/app/path/rufus.app"}}
+
+  context 'initializing driver for iOS device' do
+    it 'can create a selenium driver for an iOS device' do
+      Selenium::WebDriver.should_receive(:for).with(:remote, :desired_capabilities => capabilities, :url => config["url"])
+      Rufus::Drivers::IOS_Device.new config
+    end
+  end
+end
