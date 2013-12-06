e3d10356d0ed3659a198aa379fd2936d20acfee3
Jeremy Stewart <jlstewart379@gmail.com>
2013-12-05 22:49:55 -0500

    passing specs for finding nested views

--- lib/rufus/parser.rb
+++ lib/rufus/parser.rb
@@ -9,39 +9,62 @@ module Rufus
     end
 
     def class_for(label)
-      nil
       @screen_data['type'] if found?(label)
     end
 
     def value(label)
-      nil
       @screen_data['value'] if found?(label)
     end
 
     def width(label)
-      nil
       @screen_data['rect']['size']['width'] if found?(label)
     end
 
     def height(label)
-      nil
       @screen_data['rect']['size']['height'] if found?(label)
     end
 
     def x_pos(label)
-      nil
       @screen_data['rect']['origin']['x'] if found?(label)
     end
 
     def y_pos(label)
-      nil
       @screen_data['rect']['origin']['y'] if found?(label)
     end
 
-    private
-    def strip_escapes(json)
-      json.gsub
+    def enabled?(label)
+      @screen_data['enabled']
     end
+
+    def visible?(label)
+      @screen_data['visible']
+    end
+
+    def child_count(label)
+      @screen_data['children'].count if found(label)
+    end
+
+    def view_by_label(label)
+      @found_view = nil
+      view_data_for(@screen_data, label)
+      @found_view
+    end
+
+    def view_data_for(json, label)
+      if json['name'].eql?(label)
+        @found_view = json
+      else
+        if json['children'].count > 0
+          json['children'].each do |child|
+            view_data_for(child, label)
+          end
+        end
+      end
+    end
+
+    private
+
+
 
     def found?(label)
       @screen_data['name'].eql?(label)
